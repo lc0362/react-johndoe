842ca2eb675108024b288820d9dc72cc3eed4d36
laurac <laurac0362@gmail.com>
2024-10-14 11:23:23 +0200

    start

---
+++ src/pages/MentionsLegales.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MentionsLegales() {
+  return (
+    <div>
+      <h1>Mentions Legales</h1>
+      <p>lorem</p>
+    </div>
+  );
+}
+
+export default MentionsLegales;
