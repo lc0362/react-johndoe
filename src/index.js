80ed56944f163b0eddefdd78996d87a7717542b2
laurac <laurac0362@gmail.com>
2024-10-14 15:17:41 +0200

    modif menu link

--- src/index.js
+++ src/index.js
@@ -1,22 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; 
 import './index.css';
 import App from './App';
-import Home from './pages/Home';
-import Blog from './pages/Blog';
-import Contact from './pages/Contact';
-import MentionsLegales from './pages/MentionsLegales';
-import Realisations from './pages/Realisations';
-import Services from './pages/Services';
-
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
