2bcb9072a3ff4fb68d4ad4c74cde02f25ec6548d
laurac <laurac0362@gmail.com>
2024-10-15 19:28:48 +0200

    style changes and mentions legales

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,17 +11,17 @@ function Footer() {
             <p className="title fw-bold">John Doe</p>
             <address><div>40 Rue Laure Diebold</div>
             <div>69009 Lyon, France</div></address>
-            <div>Téléphone : <a href="tel:0620304050">06 20 30 40 50</a></div>
+            <div>Téléphone : <a href="tel:0620304050" className='text-black'>06 20 30 40 50</a></div>
             {/* icones réseaux sociaux */}
             <div className="d-flex py-3">
   <a href="https://www.github.com" title="Github icons created by Idealogo Studio - Flaticon" className="px-2">
-    <img src="/github-sign.png" alt="Icône GitHub" width="40" height="auto" />
+    <img src="/github-sign.png" alt="Icône GitHub" width="40" height="auto" id='icon-sm'/>
   </a>
   <a href="https://www.x.com" title="Twitter icons created by Alfredo Creates - Flaticon"className="px-2">
-    <img src="/twitter-sign.png" alt="Icône Twitter" width="40" height="auto" />
+    <img src="/twitter-sign.png" alt="Icône Twitter" width="40" height="auto" id='icon-sm'/>
   </a>
   <a href="https://www.linkedin.com" title="LinkedIn icons created by Alfredo Creates - Flaticon"className="px-2">
-    <img src="/linkedin-sign.png" alt="Icône LinkedIn" width="40" height="auto" />
+    <img src="/linkedin-sign.png" alt="Icône LinkedIn" width="40" height="auto" id='icon-sm'/>
   </a>
 </div>
 
@@ -31,11 +31,11 @@ function Footer() {
           <div className="col-sm-6 col-lg-3 py-1">
             <p className="title fw-bold">Liens utiles</p>
             <ul className="list-unstyled ">
-              <li><a href="/"><span className="text-primary fw-bold">&gt;</span> Accueil</a></li>
-              <li><a href="/" className=" text-auto"><span className="text-primary fw-bold">&gt;</span> À propos</a></li>
-              <li><a href="/services"><span className="text-primary fw-bold">&gt;</span> Services</a></li>
-              <li><a href="/contact"><span className="text-primary fw-bold">&gt;</span> Me contacter</a></li>
-              <li><a href="/mentionslegales"><span className="text-primary fw-bold">&gt;</span> Mentions Légales</a></li>
+              <li><span className="text-primary fw-bold">&gt;</span><a href="/" className='text-black'> Accueil</a></li>
+              <li><span className="text-primary fw-bold">&gt;</span><a href="/"className='text-black'> À propos</a></li>
+              <li><span className="text-primary fw-bold">&gt;</span><a href="/services" className='text-black'> Services</a></li>
+              <li><span className="text-primary fw-bold">&gt;</span><a href="/contact" className='text-black'> Me contacter</a></li>
+              <li><span className="text-primary fw-bold">&gt;</span><a href="/mentionslegales" className='text-black'> Mentions Légales</a></li>
             </ul>
           </div>
 
@@ -43,9 +43,9 @@ function Footer() {
           <div className="col-sm-6 col-lg-3 py-1">
             <p className="title fw-bold">Mes dernières réalisations</p>
             <ul className="list-unstyled">
-              <li><a href="/realisations"><span className="text-primary fw-bold">&gt;</span> Fresh Food</a></li>
-              <li><a href="/realisations"><span className="text-primary fw-bold">&gt;</span> Restaurant Akira</a></li>
-              <li><a href="/realisations"><span className="text-primary fw-bold">&gt;</span> Espace bien-être</a></li>
+              <li><span className="text-primary fw-bold">&gt;</span><a href="/realisations" className='text-black'> Fresh Food</a></li>
+              <li><span className="text-primary fw-bold">&gt;</span><a href="/realisations" className='text-black'> Restaurant Akira</a></li>
+              <li><span className="text-primary fw-bold">&gt;</span><a href="/realisations" className='text-black'> Espace bien-être</a></li>
             </ul>
           </div>
 
@@ -53,9 +53,9 @@ function Footer() {
           <div className="col-sm-6 col-lg-3 py-1">
             <p className="title fw-bold">Mes derniers articles</p>
             <ul className="list-unstyled">
-              <li><a href="/blog"><span className="text-primary fw-bold">&gt;</span> Coder son site en HTML/CSS</a></li>
-              <li><a href="/blog"><span className="text-primary fw-bold">&gt;</span> Vendre ses produits sur le web</a></li>
-              <li><a href="/blog"><span className="text-primary fw-bold">&gt;</span> Se positionner sur Google</a></li>
+              <li><span className="text-primary fw-bold">&gt;</span><a href="/blog" className='text-black'> Coder son site en HTML/CSS</a></li>
+              <li><span className="text-primary fw-bold">&gt;</span><a href="/blog" className='text-black'> Vendre ses produits sur le web</a></li>
+              <li><span className="text-primary fw-bold">&gt;</span><a href="/blog" className='text-black'> Se positionner sur Google</a></li>
             </ul>
           
         </div>
